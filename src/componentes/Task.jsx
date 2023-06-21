5fb6024a7d9e9940a4dd9be74710e204a77e818b
Camilo Estrada <camilo.estradas@udea.edu.co>
2023-06-21 07:10:23 -0500

    edit function

--- src/componentes/Task.jsx
+++ src/componentes/Task.jsx
@@ -1,25 +1,58 @@
-export const Task=({task,toggleTask,deleteTask})=>{
-    return(
-        <>
-                <li className="list-item task-container" >
-                    <input 
-                        type="checkbox" 
-                        checked={task.done}
-                        onChange={()=>toggleTask(task)} />
-                        <span 
-                            style={task.done? {textDecoration:"line-through"}:{textDecoration:"none"}} >
-                                {task.name}
-                        </span>
-                        <div className="button-container">
-                            <button>
-                                <i className="fa-sharp fa-solid fa-pen-to-square"></i>
-                            </button>
-                            <button onClick={()=>deleteTask(task)}>
-                                <i className="fa-sharp fa-solid fa-trash"></i>
-                            </button>
-                        </div>
-                </li>
-            
-        </>
-    )
-}
+import { useState } from "react";
+
+export const Task = ({ task, toggleTask, deleteTask,editTask }) => {
+  /* crep in estado para saber si el usuario estara en modo edicion o no de la tarea */
+  const [editing, setEditing] = useState(false);
+
+  /*funcion que ira en el onClick del boton editar para actualizar el estado y entrar en modo edicion */
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleEditingDone=(event)=>{
+    if(event.key=='Enter'){
+        setEditing(false);
+    }
+  }
+
+  /*el modo edicion solo se hara cuando el estado eiditin es true */
+  /*se crea un if para verificar cuando el estado esta en modo edicion o no */
+  let viewMode = {};
+  let editMode = {};
+
+  if (editing) {
+    viewMode.display='none';
+  }else{
+    editMode.display='none';
+  }
+
+  return (
+    <>
+      <li className={`item-list ${task.done ? "checked" : ""}`}>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => toggleTask(task)}
+        />
+        
+        <div className="button-container" style={viewMode}>
+        <span>{task.name}</span>
+          <button onClick={()=>handleEditing()}>
+            <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+          </button>
+
+          <button onClick={() => deleteTask(task)}>
+            <i className="fa-sharp fa-solid fa-trash"></i>
+          </button>
+        </div>
+        <input 
+            type="text" 
+            value={task.name} 
+            className="edit-mode"
+            style={editMode} 
+            onChange={((e)=>editTask(e.target.value,task.id))}
+            onKeyDown={(e)=>handleEditingDone(e)}/>
+      </li>
+    </>
+  );
+};
--- src/componentes/TaskList.jsx
+++ src/componentes/TaskList.jsx
@@ -1,52 +1,61 @@
-import { TaskCreator } from './TaskCreator'
-import { Task } from './Task';
-import { useState,useEffect } from 'react'
+import { TaskCreator } from "./TaskCreator";
+import { Task } from "./Task";
+import { useState, useEffect } from "react";
 
+export const TaskList = () => {
+  const [taskItems, setTaskItems] = useState([]);
 
-export const TaskList=({tasks})=>{
-    
-    const [taskItems,setTaskItems]=useState([]);
-
-    const deleteTask=(taskToDelete)=>{
-        setTaskItems(taskItems.filter((t)=>t.name !== taskToDelete.name))
-
+  const deleteTask = (taskToDelete) => {
+    setTaskItems(taskItems.filter((t) => t.name !== taskToDelete.name));
+  };
+  let index = taskItems.length + 1;
+  function createNewTask(taskName) {
+    if (!taskItems.find((task) => task.name === taskName)) {
+      setTaskItems([...taskItems, { name: taskName, done: false, id: index }]);
     }
-    function createNewTask(taskName){
-        if(!taskItems.find(task=> task.name ===taskName)){
-        setTaskItems([...taskItems,{name:taskName,done:false}])
+  }
+  const toggleTask = (task) => {
+    setTaskItems(
+      taskItems.map((t) => (t.name === task.name ? { ...t, done: !t.done } : t))
+    );
+  };
+  const editTask = (editedTask, id) => {
+    setTaskItems(
+      taskItems.map((task) => (task.id === id ? { ...task, name: editedTask } : task))
+      /* taskItems.map((t) => {
+        if (t.id === id) {
+          t.name = editedTask;
         }
+        return taskItems;
+      }) */
+    );
+  };
+
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data) {
+      setTaskItems(JSON.parse(data));
     }
-    const toggleTask=(task)=>{
-        setTaskItems(
-            taskItems.map(t=> t.name === task.name ? {...t,done:!t.done}: t)
-        )
-    }
+  }, []);
 
-    useEffect(()=>{
-        let data=localStorage.getItem('tasks');
-        if (data) {
-        setTaskItems(JSON.parse(data))
-        }
-    },[])
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskItems));
+  }, [taskItems]);
 
-    useEffect(()=> {
-        localStorage.setItem('tasks', JSON.stringify(taskItems));
-    },[taskItems])
-
-    return(
-        <div className='taskList'>
-            <TaskCreator createNewTask={createNewTask}/>
-            <ul className='list-main'>
-            {
-                taskItems.map((task)=>(
-                <Task 
-                task={task} 
-                key={task.name}
-                toggleTask={toggleTask}
-                deleteTask={deleteTask} />
-                ))
-            }
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="taskList">
+      <TaskCreator createNewTask={createNewTask} />
+      <ul className="list-main">
+        {taskItems.map((task) => (
+          <Task
+            task={task}
+            key={task.id}
+            toggleTask={toggleTask}
+            deleteTask={deleteTask}
+            editTask={editTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
