fa2b8043b3df402736c6083b7dd4353f6a8b231c
Camilo Estrada <camilo.estradas@udea.edu.co>
2023-07-01 12:03:59 -0500

    useForm en edicion

--- src/componentes/Task.jsx
+++ src/componentes/Task.jsx
@@ -1,49 +1,47 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export const Task = ({ task, toggleTask, deleteTask,editTask }) => {
-  const handleDelete=()=>{
-    if(window.confirm("Are you sure you want to delete")){
-      deleteTask(task)
+export const Task = ({ task, toggleTask, deleteTask, editTask }) => {
+  const { register, handleSubmit } = useForm();
+  const handleDelete = () => {
+    if (window.confirm("Are you sure you want to delete")) {
+      deleteTask(task);
     }
-  }
-  /* crep in estado para saber si el usuario estara en modo edicion o no de la tarea */
+  };
+
   const [editing, setEditing] = useState(false);
 
-  /*funcion que ira en el onClick del boton editar para actualizar el estado y entrar en modo edicion */
   const handleEditing = () => {
     setEditing(true);
   };
 
-  const handleEditingDone=(event)=>{
-    if(event.key=='Enter'){
-        setEditing(false);
+  const handleEditingDone = (event) => {
+    if (event.key == "Enter") {
+      setEditing(false);
     }
-  }
+  };
 
-  /*el modo edicion solo se hara cuando el estado eiditin es true */
-  /*se crea un if para verificar cuando el estado esta en modo edicion o no */
   let viewMode = {};
   let editMode = {};
 
   if (editing) {
-    viewMode.display='none';
-  }else{
-    editMode.display='none';
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
   }
 
   return (
     <>
       <li className={`item-list ${task.done ? "checked" : ""}`}>
         <div className="button-container" style={viewMode}>
-        <input
-          type="checkbox"
-          checked={task.done}
-          onChange={() => toggleTask(task)}
-        />
-        
-        
-        <span>{task.name}</span>
-          <button onClick={()=>handleEditing()}>
+          <input
+            type="checkbox"
+            checked={task.done}
+            onChange={() => toggleTask(task)}
+          />
+
+          <span>{task.name}</span>
+          <button onClick={() => handleEditing()}>
             <i className="fa-sharp fa-solid fa-pen-to-square"></i>
           </button>
 
@@ -51,13 +49,16 @@ export const Task = ({ task, toggleTask, deleteTask,editTask }) => {
             <i className="fa-sharp fa-solid fa-trash"></i>
           </button>
         </div>
-        <input 
-            type="text" 
-            value={task.name} 
+        <form onSubmit={handleSubmit((data) => editTask(data.name, task.id))}>
+          <input
+            type="text"
+            defaultValue={task.name}
+            {...register("name")}
             className="edit-mode"
-            style={editMode} 
-            onChange={((e)=>editTask(e.target.value,task.id))}
-            onKeyDown={(e)=>handleEditingDone(e)}/>
+            style={editMode}
+            onKeyDown={(e) => handleEditingDone(e)}
+          />
+        </form>
       </li>
     </>
   );
---
+++ src/explicacionCode/code.md
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export const Task = ({ task, toggleTask, deleteTask,editTask }) => {
+
+  
+  const handleDelete=()=>{
+    if(window.confirm("Are you sure you want to delete")){
+      deleteTask(task)
+    }
+  }
+
+  const [editing, setEditing] = useState(false);
+
+  /*funcion que ira en el onClick del boton editar para actualizar el estado y entrar en modo edicion */
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleEditingDone=(event)=>{
+    if(event.key=='Enter'){
+        setEditing(false);
+    }
+  }
+
+  /*el modo edicion solo se hara cuando el estado eiditin es true */
+  /*se crea un if para verificar cuando el estado esta en modo edicion o no */
+  let viewMode = {};
+  let editMode = {};
+
+  if (editing) {
+    viewMode.display='none';
+  }else{
+    editMode.display='none';
+  }
+
+  return (
+    <>
+      <li className={`item-list ${task.done ? "checked" : ""}`}>
+        <div className="button-container" style={viewMode}>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => toggleTask(task)}
+        />
+        
+        
+        <span>{task.name}</span>
+          <button onClick={()=>handleEditing()}>
+            <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+          </button>
+
+          <button onClick={() => handleDelete()}>
+            <i className="fa-sharp fa-solid fa-trash"></i>
+          </button>
+        </div>
+        <input 
+            type="text" 
+            value={task.name} 
+            className="edit-mode"
+            style={editMode} 
+            onChange={((e)=>editTask(e.target.value,task.id))}
+            onKeyDown={(e)=>handleEditingDone(e)}/>
+      </li>
+    </>
+  );
+};
